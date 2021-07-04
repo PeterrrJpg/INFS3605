628202e1abac3633659e7963aa4194a3da5c80ea
PeterrrJpg <61216715+PeterrrJpg@users.noreply.github.com>
2021-07-04 16:24:00 +1000

    login verfication
    email verification success

--- application/app/src/main/java/com/example/myapplication/AuthPage.java
+++ application/app/src/main/java/com/example/myapplication/AuthPage.java
@@ -1,13 +1,20 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
@@ -40,13 +47,50 @@ public class AuthPage extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN && resultCode == RESULT_OK) {
-            launchMainPage();
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                // Init user in firebase if new
+                IdpResponse idpResponse = data.getParcelableExtra("extra_idp_response");
+                Log.e("TAG", "isVerified = " + user.isEmailVerified());
+                Log.e("TAG", "isNewUser = " + idpResponse.isNewUser());
+                if (idpResponse.isNewUser()) {
+                    user.sendEmailVerification();
+                }
+
+                if (user.isEmailVerified()) {
+                    launchMainPage();
+                } else {
+                    Toast.makeText(AuthPage.this, "Please verify your email", Toast.LENGTH_SHORT).show();
+                    AuthUI.getInstance()
+                            .signOut(getApplicationContext())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    refresh();
+                                }
+                            });
+                }
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Log.e("TAG", "error = " + response.getError().getErrorCode());
+            }
         }
     }
 
     private void launchMainPage() {
         Intent intent = new Intent(AuthPage.this, MainPage.class);
+        startActivity(intent);
+    }
+
+    private void refresh() {
+        Intent intent = new Intent(AuthPage.this, AuthPage.class);
         startActivity(intent);
     }
 }
--- application/app/src/main/java/com/example/myapplication/MainPage.java
+++ application/app/src/main/java/com/example/myapplication/MainPage.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,9 +8,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class MainPage extends AppCompatActivity {
 
     private Button btTest;
+    private Button btLogOut;
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +34,32 @@ public class MainPage extends AppCompatActivity {
                 launchTestPage();
             }
         });
+
+        btLogOut = findViewById(R.id.btLogOut);
+        btLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthUI.getInstance()
+                        .signOut(getApplicationContext())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                launchAuthPage();
+                            }
+                        });
+            }
+        });
+
+
     }
 
     private void launchTestPage() {
         Intent intent = new Intent(MainPage.this, TestPage.class);
+        startActivity(intent);
+    }
+
+    private void launchAuthPage() {
+        Intent intent = new Intent(MainPage.this, AuthPage.class);
         startActivity(intent);
     }
 }
