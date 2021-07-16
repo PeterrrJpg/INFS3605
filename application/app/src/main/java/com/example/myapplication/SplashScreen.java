f31bddbb751957369c3f86821bc7bfcf822d7f52
PeterrrJpg <61216715+PeterrrJpg@users.noreply.github.com>
2021-07-16 14:35:32 +1000

    splashscreen

---
+++ application/app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int DURATION = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, AuthPage.class);
+                startActivity(intent);
+            }
+        }, DURATION);
+    }
+}
